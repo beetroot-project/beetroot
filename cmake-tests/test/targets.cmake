c812daebdbfa933022b3a74f6dbe988fa905c262
adam <adam@statystyka.net>
2019-02-20 20:47:05 +0100

    Final fixes to tests

--- cmake-tests/test/targets.cmake
+++ cmake-tests/test/targets.cmake
@@ -12,7 +12,6 @@ set(TEMPLATE_OPTIONS
 function(generate_targets TEMPLATE_NAME)
 	file(WRITE ${CMAKE_BINARY_DIR}/main.c "int main(void){return 0;}\n")
 	add_executable(${TARGET_NAME} main.c)
-	add_test(NAME ${TARGET_NAME} COMMAND ${TARGET_NAME})
 endfunction()
 
 #663582226 - sms do 14:00 z inf. czy zgłosił się kurier
---
+++ cmake-tests/test/test/targets.cmake
@@ -0,0 +1,24 @@
+set(ENUM_TEMPLATES SELF_TEST_TEST)
+
+set(TARGET_PARAMETERS 
+)
+
+set(TEMPLATE_OPTIONS
+	NO_TARGETS
+)
+
+function(declare_dependencies TARGET_NAME)
+	build_target(SELF_TEST)
+endfunction()
+
+function(generate_targets TEMPLATE_NAME)
+	get_target(SELF_TEST SELF_TEST_DRIVER)
+
+	file(WRITE  "${CMAKE_CURRENT_BINARY_DIR}/${TARGET_NAME}.sh" "#!/bin/sh\n")
+	file(APPEND "${CMAKE_CURRENT_BINARY_DIR}/${TARGET_NAME}.sh" "./${SELF_TEST_DRIVER}.sh")
+	
+	add_test(NAME ${TARGET_NAME} COMMAND /bin/sh ${TARGET_NAME}.sh
+		WORKING_DIRECTORY "${CMAKE_CURRENT_BINARY_DIR}"
+	)
+endfunction()
+
